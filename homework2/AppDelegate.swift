83ef4a7ff786f04394184058c98e389240be08e9
Danil Albutov <albutovdanil@gmail.com>
2021-10-26 23:27:17 +0400

    initial commit; designed tapbar

---
+++ homework2/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  homework2
+//
+//  Created by Данил Албутов on 26.10.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
